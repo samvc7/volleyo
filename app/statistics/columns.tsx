15eb112c4ee0f80c4241deb371a63ff985cddb2a
Vicente Samuel Cala <vicente.cala07@gmail.com>
2025-01-07 18:18:04 +0100

    refac: extracted columns from data table & added prettier config

---
+++ app/statistics/columns.tsx
@@ -0,0 +1,153 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Column, ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+
+// Maybe use Zod schema here later
+export type Statistics = {
+  id: string
+  name: string
+  attackSuccess: number
+  attackFailures: number
+  digs: number
+  sets: number
+  blocks: number
+  errors: number
+}
+
+// TODO-next: stopped at sorting, next is filtering
+// https://ui.shadcn.com/docs/components/data-table#filtering
+
+// research: Need to use invertSorting because sortDescFirst is not working
+export const columns: ColumnDef<Statistics>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <SortableHeaderButton
+          column={column}
+          name="Name"
+        />
+      )
+    },
+  },
+  {
+    accessorKey: "attackAttempts",
+    header: "Attack Attempts",
+    accessorFn: row => row.attackSuccess + row.attackFailures,
+  },
+  {
+    accessorKey: "attackSuccess",
+    header: "Attack Success",
+  },
+  {
+    accessorKey: "attackFailures",
+    header: "Attack Failures",
+  },
+  {
+    accessorKey: "attackSuccessRate",
+    header: ({ column }) => (
+      <SortableHeaderButton
+        column={column}
+        name="Attack Success Rate"
+      />
+    ),
+    invertSorting: true,
+    accessorFn: row => {
+      const attackSuccessRateRaw = (row.attackSuccess / (row.attackSuccess + row.attackFailures)) * 100
+      const attackSuccessRate = round2DecimalPlaces(attackSuccessRateRaw)
+
+      return attackSuccessRate
+    },
+  },
+  {
+    accessorKey: "digs",
+    header: ({ column }) => (
+      <SortableHeaderButton
+        column={column}
+        name="Digs"
+      />
+    ),
+    invertSorting: true,
+  },
+  {
+    accessorKey: "sets",
+    header: "Sets",
+  },
+  {
+    accessorKey: "blocks",
+    header: ({ column }) => (
+      <SortableHeaderButton
+        column={column}
+        name="Blocks"
+      />
+    ),
+    invertSorting: true,
+  },
+  {
+    accessorKey: "errors",
+    header: ({ column }) => (
+      <SortableHeaderButton
+        column={column}
+        name="Errors"
+      />
+    ),
+    invertSorting: true,
+  },
+  {
+    id: "actions",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="h-8 w-8 p-0"
+            >
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View Player</DropdownMenuItem>
+            <DropdownMenuItem>View Statistics</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
+
+const round2DecimalPlaces = (num: number) => Math.round(num * 100) / 100
+
+type SortableHeaderButtonProps = {
+  column: Column<Statistics>
+  name: string
+}
+
+const SortableHeaderButton = ({ column, name }: SortableHeaderButtonProps) => {
+  return (
+    <Button
+      // TODO: fix hovered background color of header. Padding 0 needed because causes cell disalignment
+      className="p-0"
+      variant="ghost"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    >
+      {name}
+      <ArrowUpDown className="ml-2 h-4 w-4" />
+    </Button>
+  )
+}
--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,11 +1,76 @@
-import { StatisticsTable } from "./table";
+import { DataTable } from "@/app/statistics/data-table"
+// import { StatisticsTable } from "./table";
+import { columns, Statistics } from "@/app/statistics/columns"
 
-export default function Statistics() {
+export default async function StatisticsPage() {
+  const data = await getData()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <StatisticsTable />
+        <div className="container mx-auto py-10">
+          <DataTable
+            columns={columns}
+            data={data}
+          />
+        </div>
+        {/* <StatisticsTable /> */}
       </div>
     </main>
-  );
+  )
+}
+
+async function getData(): Promise<Statistics[]> {
+  return [
+    {
+      id: "m5gr84i9",
+      name: "Monserrat",
+      attackSuccess: 10,
+      attackFailures: 0,
+      digs: 10,
+      sets: 0,
+      blocks: 7,
+      errors: 5,
+    },
+    {
+      id: "3u1reuv4",
+      name: "Silas",
+      attackSuccess: 1,
+      attackFailures: 2,
+      digs: 20,
+      sets: 0,
+      blocks: 12,
+      errors: 7,
+    },
+    {
+      id: "derv1ws0",
+      name: "Carmella",
+      attackSuccess: 4,
+      attackFailures: 7,
+      digs: 13,
+      sets: 0,
+      blocks: 2,
+      errors: 2,
+    },
+    {
+      id: "5kma53ae",
+      name: "Kathlyn",
+      attackSuccess: 20,
+      attackFailures: 3,
+      digs: 1,
+      sets: 0,
+      blocks: 1,
+      errors: 9,
+    },
+    {
+      id: "bhqecj4p",
+      name: "Laurie",
+      attackSuccess: 7,
+      attackFailures: 4,
+      digs: 12,
+      sets: 0,
+      blocks: 1,
+      errors: 1,
+    },
+  ]
 }
