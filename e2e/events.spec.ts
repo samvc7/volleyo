5cccf015873c4d1e11eb48879e5666568195a11d
Vicente Samuel Cala <vicente.cala07@gmail.com>
2025-08-15 12:07:14 +0200

    Added e2e tests for events and members pages

--- e2e/events.spec.ts
+++ e2e/events.spec.ts
@@ -1,13 +1,10 @@
-import { DATE_FORMAT } from "@/app/utils"
 import { test, expect } from "@playwright/test"
-import { format } from "date-fns"
 
 test("navigate to team events page", async ({ page }) => {
   await page.goto("/")
   await page.getByRole("combobox", { name: "Select Team" }).click()
   await page.getByRole("option", { name: "Alpha Squad" }).click()
 
-  // await expect(page.getByRole("combobox", { name: "Select Team" })).toBeVisible()
   await expect(page.getByText("Alpha Squad")).toBeVisible()
   await expect(page.getByText("Past Events")).toBeVisible()
   await expect(page.getByText("Game")).toHaveCount(5)
@@ -16,7 +13,7 @@ test("navigate to team events page", async ({ page }) => {
   await expect(page.getByRole("link", { name: "Members" })).toBeVisible()
 })
 
-test.skip("create event", async ({ page }) => {
+test("create", async ({ page }) => {
   await page.goto("/alpha-squad/events")
 
   await page.getByRole("button", { name: "Add Event" }).click()
@@ -30,8 +27,8 @@ test.skip("create event", async ({ page }) => {
     .getByRole("textbox", { name: "Description" })
     .fill("This is a test event created by Playwright E2E tests.")
 
-  // TODO: update DatePicker version & test date selection
-  // await page.getByRole("textbox", { name: "Date" }).fill(format(new Date(), DATE_FORMAT))
+  await page.getByRole("textbox", { name: "Date" }).click()
+  await page.getByRole("button", { name: "today" }).click()
 
   await page.getByRole("textbox", { name: "Time" }).fill("10:00")
   await page.getByRole("textbox", { name: "Location" }).fill("Test Location")
@@ -39,12 +36,31 @@ test.skip("create event", async ({ page }) => {
   await page.getByRole("button", { name: "Save" }).click()
 
   await expect(page.getByText("E2E Test")).toBeVisible()
-  // past events,
 })
 
-test.skip("filter events by date range", async ({ page }) => {
-  await page.goto("/alpha-squad/events")
-  // Add logic to filter events by date range
+test("edit event", async ({ page }) => {
+  await page.goto("/event/game-5")
+  await page.getByRole("button", { name: "Edit Event" }).click()
+  await page.getByRole("textbox", { name: "Title" }).fill("Game 5 - Edited")
+  await page.getByRole("textbox", { name: "Description" }).fill("Playoffs - edited")
+  await page.getByRole("textbox", { name: "Location" }).fill("New Location")
+  await page.getByRole("textbox", { name: "Opponent" }).fill("New Opponent")
+  await page.getByRole("spinbutton", { name: "Team Score" }).fill("25")
+  await page.getByRole("spinbutton", { name: "Opponent Score" }).fill("20")
+
+  await page.getByRole("button", { name: "Save" }).click()
+
+  await expect(page.getByRole("heading", { name: "Game 5 - Edited" })).toBeVisible()
+  await expect(page.getByText("Playoffs - edited")).toBeVisible()
+  await expect(page.getByText("New Location")).toBeVisible()
+  await expect(page.getByText("New Opponent")).toBeVisible()
+  await expect(page.getByText("25", { exact: true })).toBeVisible()
+  await expect(page.getByText("20", { exact: true })).toBeVisible()
+})
+
+test("filter events by date range", async ({ page }) => {
+  await page.goto("/alpha-squad/events?from=2025-08-06&to=2025-08-08")
+  await expect(page.getByText("Game")).toHaveCount(2)
 })
 
 test("navigate to event details", async ({ page }) => {
--- e2e/members.spec.ts
+++ e2e/members.spec.ts
@@ -8,3 +8,33 @@ test("navigate to members page", async ({ page }) => {
   await expect(page.getByRole("cell", { name: "Name" })).toBeVisible()
   await expect(page.getByRole("cell", { name: "Email" })).toBeVisible()
 })
+
+test("create, edit, remove team member", async ({ page }) => {
+  await page.goto("/alpha-squad/members")
+
+  // Add member
+  await page.getByRole("button", { name: "Add Member" }).click()
+  await expect(page.getByRole("heading", { name: "Add Team Member" })).toBeVisible()
+  await page.getByRole("textbox", { name: "First Name" }).fill("John")
+  await page.getByRole("textbox", { name: "Last Name" }).fill("Doe")
+  await page.getByRole("textbox", { name: "Email" }).fill("john.doe@example.com")
+  await page.getByRole("button", { name: "Create" }).click()
+  await page.getByRole("textbox", { name: "Filter names" }).fill("John")
+  await expect(page.getByRole("cell", { name: "John Doe" })).toBeVisible()
+
+  // Edit member
+  await page.getByRole("row", { name: "John Doe" }).getByRole("button").click()
+  await page.getByRole("menuitem", { name: "Edit" }).click()
+  await expect(page.getByRole("heading", { name: "Edit Team Member" })).toBeVisible()
+  await page.getByRole("textbox", { name: "Nick Name" }).fill("Junny")
+  await page.getByRole("button", { name: "Save" }).click()
+  await page.getByRole("textbox", { name: "Filter names" }).fill("Junny")
+  await expect(page.getByRole("cell", { name: "Junny" })).toBeVisible()
+
+  // Remove member
+  await page.getByRole("row", { name: "Junny" }).getByRole("button").click()
+  await page.getByRole("menuitem", { name: "Remove" }).click()
+  await expect(page.getByRole("heading", { name: "Removing Team Member" })).toBeVisible()
+  await page.getByRole("button", { name: "Continue" }).click()
+  await expect(page.getByRole("cell", { name: "No Results" })).toBeVisible()
+})
